6da6a17ece6c495e024af26611384d55fc0d50a8
Luis Souza <luis_csouza@uol.com.br>
2019-06-20 17:07:34 -0300

    primeiro

---
+++ ASPNETCORE/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPNETCOREMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETCOREMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            CarregarListaDeDados();
+            return View();
+        }
+
+        public void CarregarListaDeDados()
+            {
+                HomeModel objhomeModel = new HomeModel();
+            ViewBag.Lista = objhomeModel.Getall();
+            }
+    }
+}
---
+++ ASPNETCORE/Models/HomeModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCOREMVC.Models
+{
+    public class HomeModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public List<HomeModel> Getall()
+        {
+            HomeModel item;
+
+            List<HomeModel> Lista = new List<HomeModel>();
+
+            item = new HomeModel();
+            item.Id = 1;
+            item.Nome = "luis";
+            Lista.Add(item);
+
+            item = new HomeModel();
+            item.Id = 2;
+            item.Nome = "Mauricio";
+            Lista.Add(item);
+
+            return Lista;
+        }
+    }
+}
