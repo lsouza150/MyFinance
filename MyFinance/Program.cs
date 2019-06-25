fcf819f6826e4568664dd1f748599db9d1bb3333
Luis Souza <luis.souza@poliview.com.br>
2019-06-25 07:36:10 -0300

    Segunda

--- MyFinance/Program.cs
+++ MyFinance/Program.cs
@@ -20,5 +20,8 @@ namespace MyFinance
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+              
+                
+
     }
 }
